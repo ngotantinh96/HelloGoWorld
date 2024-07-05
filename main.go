d6928583a94693bbfd85be4b1df2912f91bf93ed
ngotantinh96 <ngotantinh96@gmail.com>
2024-07-05 10:34:52 +0700

    feat: simple APIs with json encoding

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, Go World!")
+	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/json", helloJSONHandler)
+	fmt.Println("Starting server with address: http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
+
+func helloHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(responseWriter, "Hello, Go World!")
+}
+
+func helloJSONHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	msg := Message{
+		Greeting: "Hello, Go World in JSON!",
+	}
+	responseWriter.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(responseWriter).Encode(msg)
+}
+
+type Message struct {
+	Greeting string `json:"greeting"`
 }
