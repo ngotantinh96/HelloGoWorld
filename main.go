810b8631eac9ea6658ed96609d14b8ca3e359691
ngotantinh96 <ngotantinh96@gmail.com>
2024-07-05 10:43:22 +0700

    feat: implement request routing and logging middleware using gorilla/mux

--- main.go
+++ main.go
@@ -4,27 +4,38 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", helloHandler)
-	http.HandleFunc("/json", helloJSONHandler)
+	r := mux.NewRouter()
+	r.Use(loggingMiddleware)
+	r.HandleFunc("/", helloHandler).Methods("GET")
+	r.HandleFunc("/json", helloJSONHandler).Methods("GET")
 	fmt.Println("Starting server with address: http://localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Println("Error starting server:", err)
 	}
 }
 
-func helloHandler(responseWriter http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(responseWriter, "Hello, Go World!")
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Go World!")
 }
 
-func helloJSONHandler(responseWriter http.ResponseWriter, request *http.Request) {
+func helloJSONHandler(w http.ResponseWriter, r *http.Request) {
 	msg := Message{
 		Greeting: "Hello, Go World in JSON!",
 	}
-	responseWriter.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(responseWriter).Encode(msg)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(msg)
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Request received: ", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
 
 type Message struct {
